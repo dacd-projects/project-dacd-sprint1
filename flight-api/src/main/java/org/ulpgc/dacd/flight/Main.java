9ce28da886d96f5417db6553868f55a3dae56613
armas <armassniraa@gmail.com>
2026-04-06 19:21:23 +0100

    create tables automatically when starting the application and add Jackson dependency for JSON parsing

--- flight-api/src/main/java/org/ulpgc/dacd/flight/Main.java
+++ flight-api/src/main/java/org/ulpgc/dacd/flight/Main.java
@@ -1,11 +1,18 @@
 package org.ulpgc.dacd.flight;
 
+import org.ulpgc.dacd.persistence.SchemaInitializer;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        try {
+            SchemaInitializer.createTables();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         FlightService service = new FlightService();
         service.execute();
+
         System.out.println("Sistema funcionando correctamente");
     }
 }
