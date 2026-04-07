f277ab3e4f2d420cfd3dea657a7829ab0c401dda
armas <armassniraa@gmail.com>
2026-04-07 12:09:56 +0100

    add captured_at and new fields in flight processing

--- flight-api/src/main/java/org/ulpgc/dacd/flight/FlightService.java
+++ flight-api/src/main/java/org/ulpgc/dacd/flight/FlightService.java
@@ -15,6 +15,8 @@ public class FlightService {
 
             String json = client.fetchFlights();
 
+            long capturedAt = System.currentTimeMillis();
+
             ObjectMapper mapper = new ObjectMapper();
             JsonNode root = mapper.readTree(json);
             JsonNode states = root.get("states");
@@ -23,20 +25,29 @@ public class FlightService {
 
                 for (JsonNode state : states) {
 
-                    if (state != null && state.size() > 7) {
+                    if (state != null && state.size() > 9) {
 
                         String icao = state.get(0).asText();
                         String callsign = state.get(1).asText();
                         String country = state.get(2).asText();
+
                         long lastUpdate = state.get(4).asLong();
+
+                        double longitude = state.get(5).asDouble();
+                        double latitude = state.get(6).asDouble();
                         double altitude = state.get(7).asDouble();
+                        double velocity = state.get(9).asDouble();
 
                         repository.saveFlight(
                                 icao,
                                 callsign,
                                 country,
+                                latitude,
+                                longitude,
                                 altitude,
-                                lastUpdate
+                                velocity,
+                                lastUpdate,
+                                capturedAt
                         );
                     }
                 }
