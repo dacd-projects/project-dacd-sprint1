c05a921d7929e489cc0016c59a3f82f07d88ba20
adriistna <adriansanros05@gmail.com>
2026-04-15 16:59:55 +0100

    add Main class that creates and connects all weather module components

---
+++ weather-api/src/main/java/org/ulpgc/dacd/Main.java
@@ -0,0 +1,18 @@
+package org.ulpgc.dacd;
+
+import org.ulpgc.dacd.control.NasaFeeder;
+import org.ulpgc.dacd.control.SpaceWeatherClient;
+import org.ulpgc.dacd.control.SpaceWeatherController;
+import org.ulpgc.dacd.control.SpaceWeatherStore;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        NasaFeeder feeder = new SpaceWeatherClient();
+        SpaceWeatherStore store = new SqliteSpaceWeatherStore();
+        SpaceWeatherController controller = new SpaceWeatherController(feeder, store);
+        controller.run();
+
+    }
+}
