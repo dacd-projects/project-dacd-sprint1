0a9182640cdb49bef94b0b095b1f283dadf1174f
armas <armassniraa@gmail.com>
2026-04-22 18:48:16 +0100

    switch Main to use ActiveMQFlightStore as publisher

--- flight-api/src/main/java/org/ulpgc/dacd/Main.java
+++ flight-api/src/main/java/org/ulpgc/dacd/Main.java
@@ -1,21 +1,17 @@
 package org.ulpgc.dacd;
 
+import org.ulpgc.dacd.control.ActiveMQFlightStore;
 import org.ulpgc.dacd.control.FlightController;
 import org.ulpgc.dacd.control.FlightFeeder;
 import org.ulpgc.dacd.control.FlightStore;
 import org.ulpgc.dacd.control.OpenSkyFlightFeeder;
-import org.ulpgc.dacd.control.SqliteFlightStore;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
 
     public static void main(String[] args) {
 
         FlightFeeder feeder = new OpenSkyFlightFeeder();
-        FlightStore store = new SqliteFlightStore();
+        FlightStore store = new ActiveMQFlightStore();
         FlightController controller = new FlightController(feeder, store);
         controller.run();
 
