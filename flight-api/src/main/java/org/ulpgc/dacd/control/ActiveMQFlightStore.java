733742ca023b4825fa9323a6cd7b1037f3afda49
armas <armassniraa@gmail.com>
2026-04-27 21:38:06 +0100

    refactor ActiveMQFlightStore to reuse connection and fix Instant serialization

--- flight-api/src/main/java/org/ulpgc/dacd/control/ActiveMQFlightStore.java
+++ flight-api/src/main/java/org/ulpgc/dacd/control/ActiveMQFlightStore.java
@@ -2,10 +2,9 @@ package org.ulpgc.dacd.control;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import jakarta.jms.*;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.ulpgc.dacd.model.Flight;
-
-import jakarta.jms.*;
 
 public class ActiveMQFlightStore implements FlightStore {
 
@@ -14,29 +13,38 @@ public class ActiveMQFlightStore implements FlightStore {
     private static final String SOURCE_ID = "flight-api";
 
     private final Gson gson = new Gson();
+    private Session session;
+    private MessageProducer producer;
 
-    @Override
-    public void save(Flight flight) {
+    public ActiveMQFlightStore() {
         try {
             ConnectionFactory factory = new ActiveMQConnectionFactory(BROKER_URL);
             Connection connection = factory.createConnection();
             connection.start();
-
-            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
             Destination destination = session.createTopic(TOPIC_NAME);
-            MessageProducer producer = session.createProducer(destination);
+            producer = session.createProducer(destination);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-            JsonObject event = gson.toJsonTree(flight).getAsJsonObject();
+    @Override
+    public void save(Flight flight) {
+        try {
+            JsonObject event = new JsonObject();
+            event.addProperty("icao", flight.getIcao());
+            event.addProperty("callsign", flight.getCallsign());
+            event.addProperty("country", flight.getCountry());
+            event.addProperty("latitude", flight.getLatitude());
+            event.addProperty("longitude", flight.getLongitude());
+            event.addProperty("altitude", flight.getAltitude());
+            event.addProperty("velocity", flight.getVelocity());
+            event.addProperty("lastUpdate", flight.getLastUpdate().toString());
             event.addProperty("ts", flight.getCapturedAt());
             event.addProperty("ss", SOURCE_ID);
-
             TextMessage message = session.createTextMessage(event.toString());
             producer.send(message);
-
-            producer.close();
-            session.close();
-            connection.close();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
