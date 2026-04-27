dba5b9bcd46c56b8736adcccba323d6f36dc33e9
armas <armassniraa@gmail.com>
2026-04-27 21:38:49 +0100

    refactor ActiveMQSpaceWeatherStore to reuse connection

--- weather-api/src/main/java/org/ulpgc/dacd/control/ActiveMQSpaceWeatherStore.java
+++ weather-api/src/main/java/org/ulpgc/dacd/control/ActiveMQSpaceWeatherStore.java
@@ -2,10 +2,9 @@ package org.ulpgc.dacd.control;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import jakarta.jms.*;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.ulpgc.dacd.model.SpaceWeather;
-
-import jakarta.jms.*;
 
 public class ActiveMQSpaceWeatherStore implements SpaceWeatherStore {
 
@@ -14,28 +13,35 @@ public class ActiveMQSpaceWeatherStore implements SpaceWeatherStore {
     private static final String SOURCE_ID = "weather-api";
 
     private final Gson gson = new Gson();
+    private Session session;
+    private MessageProducer producer;
 
-    @Override
-    public void save(SpaceWeather event) {
+    public ActiveMQSpaceWeatherStore() {
         try {
             ConnectionFactory factory = new ActiveMQConnectionFactory(BROKER_URL);
             Connection connection = factory.createConnection();
             connection.start();
-
-            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
             Destination destination = session.createTopic(TOPIC_NAME);
-            MessageProducer producer = session.createProducer(destination);
+            producer = session.createProducer(destination);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-            JsonObject json = gson.toJsonTree(event).getAsJsonObject();
+    @Override
+    public void save(SpaceWeather event) {
+        try {
+            JsonObject json = new JsonObject();
+            json.addProperty("kpIndex", event.getKpIndex());
+            json.addProperty("startTime", event.getStartTime().toString());
+            json.addProperty("endTime", event.getEndTime().toString());
+            json.addProperty("capturedAt", event.getCapturedAt());
             json.addProperty("ts", event.getCapturedAt());
             json.addProperty("ss", SOURCE_ID);
 
             TextMessage message = session.createTextMessage(json.toString());
             producer.send(message);
-
-            producer.close();
-            session.close();
-            connection.close();
 
         } catch (Exception e) {
             e.printStackTrace();
