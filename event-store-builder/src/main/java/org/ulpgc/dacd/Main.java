328e59644f4d8c1f805dab6ae4726fc488f1e89e
armas <armassniraa@gmail.com>
2026-04-27 21:45:20 +0100

    update startup parameters for tcp connection and Flight/SpaceWeather topics

--- event-store-builder/src/main/java/org/ulpgc/dacd/Main.java
+++ event-store-builder/src/main/java/org/ulpgc/dacd/Main.java
@@ -4,7 +4,11 @@ import org.ulpgc.dacd.control.EventStoreBuilder;
 
 public class Main {
     public static void main(String[] args) {
-        EventStoreBuilder builder = new EventStoreBuilder();
+        String brokerUrl = args[0];
+        String clientId = args[1];
+        String[] topics = args[2].split(",");
+
+        EventStoreBuilder builder = new EventStoreBuilder(brokerUrl, clientId, topics);
         builder.start();
     }
 }
