4343db0d1dd488829d4f2870d8918ec37503f91a
adriistna <adriansanros05@gmail.com>
2026-05-01 23:53:41 +0100

    wire all components in Main with batch and speed layer startup

--- business-unit/src/main/java/org/ulpgc/dacd/Main.java
+++ business-unit/src/main/java/org/ulpgc/dacd/Main.java
@@ -1,7 +1,29 @@
 package org.ulpgc.dacd;
 
+import org.ulpgc.dacd.control.AlertService;
+import org.ulpgc.dacd.control.DatamartUpdater;
+import org.ulpgc.dacd.control.EventStoreReader;
+import org.ulpgc.dacd.control.TopicSubscriber;
+import org.ulpgc.dacd.rest.RestApi;
+import org.ulpgc.dacd.store.DatamartStore;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Business Unit starting...");
+        String brokerUrl = args[0];
+        String eventStorePath = args[1];
+        String[] topics = {"Flight", "SpaceWeather"};
+
+        DatamartStore datamartStore = new DatamartStore();
+        DatamartUpdater datamartUpdater = new DatamartUpdater(datamartStore);
+
+        EventStoreReader eventStoreReader = new EventStoreReader(eventStorePath, datamartUpdater);
+        eventStoreReader.loadHistory();
+
+        TopicSubscriber subscriber = new TopicSubscriber(brokerUrl, topics, datamartUpdater);
+        subscriber.start();
+
+        AlertService alertService = new AlertService(datamartStore);
+        RestApi restApi = new RestApi(datamartStore, alertService);
+        restApi.start(8080);
     }
 }
